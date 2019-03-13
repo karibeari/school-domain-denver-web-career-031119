605d71587474dd4d365b35d5fa32c9308ad1676a
github username <github email address>
2019-03-13 13:08:04 -0600

    completed school domain

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,23 @@
-# code here!
+require 'pry'
+
+class School
+
+  def initialize(school_name)
+    @school_name = school_name
+    @roster = {}
+  end
+
+  attr_reader :roster
+
+  def add_student(name, grade)
+    @roster.has_key?(grade) ? @roster[grade]<<name : @roster[grade]=[name]
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    @roster.each {|grade, names| names.sort!}
+  end
+end
